85e279a105ee3489c53fce858b5eaeef164be70e
motojate <120537554+motojate@users.noreply.github.com>
2023-08-18 18:31:15 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 47.80 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,13 +1,4 @@
 function solution(arr)
 {
-    var answer = [arr[0]];
-    let tmp = arr[0]
-    for(let i = 1; i < arr.length; i++) {
-        if(arr[i] === tmp) continue
-        else answer.push(arr[i])
-        
-        tmp = arr[i]
-    }
-    
-    return answer;
+    return arr.filter((data, idx) => data !== arr[idx - 1]);
 }
