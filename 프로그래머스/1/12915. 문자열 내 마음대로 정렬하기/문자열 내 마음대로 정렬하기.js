bf7e776083073e79f95fde7693d916a58138a24d
강대현 <120537554+motojate@users.noreply.github.com>
2024-01-25 18:33:42 +0900

    Update 문자열 내 마음대로 정렬하기.js

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -3,9 +3,5 @@ function solution(strings, n) {
     const sortArr = strings.sort().map((val, idx) => [val[n], idx]).sort((a, b) => a[0].localeCompare(b[0])).forEach((data) => {
         answer.push(strings[data[1]]);
     });
-    
-    
-    
-   
     return answer;
 }
