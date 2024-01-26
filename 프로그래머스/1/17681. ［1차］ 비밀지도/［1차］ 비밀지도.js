077d8c93bb1efcb700eb5b7792ba0cb612c92a79
강대현 <120537554+motojate@users.noreply.github.com>
2024-01-26 12:34:56 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -0,0 +1,24 @@
+function solution(n, arr1, arr2) {
+    var answer = [];
+    const binaryNumArr = [];
+    for(let i = 0; i < n; i++) {
+        const binaryNumArr1 = convertBinaryNum(arr1[i].toString(2), n);
+        const binaryNumArr2 = convertBinaryNum(arr2[i].toString(2), n);
+        const orOperateNum = arr1[i] | arr2[i];
+        binaryNumArr.push(convertBinaryNum(orOperateNum.toString(2), n));
+    }
+    binaryNumArr.forEach((data) => {
+        answer.push(convertWall(data));
+    })
+    return answer;
+}
+
+const convertBinaryNum = (binaryNum, n) => {
+    if(binaryNum.length < n) binaryNum = "0".repeat(n - binaryNum.length) + binaryNum;
+    return binaryNum
+}
+
+const convertWall = (str) => {
+    const newString = str.replaceAll("1", "#");
+    return newString.replaceAll("0", " ");
+}
