4807e5fa07f8e91cc01d47c2e30f92dec09139f1
강대현 <120537554+motojate@users.noreply.github.com>
2024-01-28 18:17:52 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -0,0 +1,14 @@
+function solution(k, score) {
+    const buffer = [];
+    
+    
+    return score.map((data, idx) => {
+        if(idx < k) buffer.push(data);
+        else {
+            buffer.sort((a, b) => a - b);
+            const minResult = buffer[0];
+            if(data >= minResult) buffer[0] = data;
+        }
+        return Math.min(...buffer);
+    });
+}
