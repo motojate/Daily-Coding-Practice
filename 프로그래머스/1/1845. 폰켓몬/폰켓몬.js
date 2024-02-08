65c7d1f7911243f3b2f1036b292ed052b46de779
강대현 <120537554+motojate@users.noreply.github.com>
2024-02-09 00:56:29 +0900

    [level 1] Title: 폰켓몬, Time: 0.24 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,6 +1,5 @@
 function solution(nums) {
-    var answer = 0;
-    const numsSetLength = [...new Set(nums)].length;
+    const numsSetLength = new Set(nums).size;
     const N = Math.floor(nums.length / 2);
     return numsSetLength > N ? N : numsSetLength;
 }
