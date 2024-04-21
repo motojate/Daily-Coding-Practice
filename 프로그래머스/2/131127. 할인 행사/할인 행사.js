21b9cacb164418de714a719227acdb23270efcdf
강대현 <120537554+motojate@users.noreply.github.com>
2024-04-22 01:13:05 +0900

    [level 2] Title: 할인 행사, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,23 +1,24 @@
 function solution(want, number, discount) {
-    const mapper = new Map();
+    const discountMap = new Map();
+
     let day = 0;
     let answer = 0;
     
     discount.slice(0, 10).forEach((v) => {
-        const vCount = mapper.get(v);
-        if(vCount) mapper.set(v, vCount + 1);
-        else mapper.set(v, 1);
+        const vCount = discountMap.get(v);
+        if(vCount) discountMap.set(v, vCount + 1);
+        else discountMap.set(v, 1);
     })
     
     const setMapper = (prevItem, nextItem) => {
-        mapper.set(prevItem, mapper.get(prevItem) - 1);
-        mapper.set(nextItem, mapper.get(nextItem) ? mapper.get(nextItem) + 1 : 1);
+        discountMap.set(prevItem, discountMap.get(prevItem) - 1);
+        discountMap.set(nextItem, discountMap.get(nextItem) ? discountMap.get(nextItem) + 1 : 1);
     }
     
     for(let i = 10; i <= discount.length; i++) {
         let flag = true;
         for(let j = 0; j < want.length; j++) {
-            if((mapper.get(want[j]) ?? 0) < number[j]) flag = false;
+            if((discountMap.get(want[j]) ?? 0) < number[j]) flag = false;
         }
         if(flag) answer++;
         
