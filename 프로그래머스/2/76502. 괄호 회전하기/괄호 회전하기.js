7c025504ccb66ab1240332a3e17c880621b837fa
강대현 <120537554+motojate@users.noreply.github.com>
2024-04-22 18:27:55 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,7 +1,30 @@
 function solution(s) {
-    var answer = -1;
-    for(const str of s) {
-        console.log(str);
+    // if(s.length % 2 === 1) return 0;
+    let answer = 0;
+    const mapperArray = ['{', '(', '['];
+    const mapper = {
+        '{': '}',
+        '(': ')',
+        '[': ']'
+    };
+    const stack = [];
+    let newStr = s;
+    for(let i = 0; i < s.length; i++) {
+        const strBuffer = [...newStr];
+        while(strBuffer.length > 0) {
+            if(strBuffer.length === 1 && stack.length === 0) break;
+            const peak = strBuffer.pop();
+            if(mapperArray.includes(peak)) {
+                if(stack.length === 0 || mapper[peak] !== stack.pop()) break;
+            } else stack.push(peak);
+        }
+        if(strBuffer.length === 0 && stack.length === 0) answer++;
+        newStr = convertStr(newStr);
     }
+
     return answer;
+}
+
+const convertStr = (str) => {
+    return `${str.slice(1)}${str[0]}`
 }
