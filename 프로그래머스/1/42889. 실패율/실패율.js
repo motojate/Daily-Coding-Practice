10af148d8e63e6f34b717fad92bf8f69df887cea
강대현 <120537554+motojate@users.noreply.github.com>
2024-04-25 15:55:12 +0900

    [level 1] Title: 실패율, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,9 +1,20 @@
 function solution(N, stages) {
     var answer = [];
     for(let i = 1; i <= N; i++) {
+        
+        const rangeArr = stages.reduce((acc, item) => {
+            if(item === i) {
+                acc[0].push(item);
+                acc[1].push(item);
+            }
+            if(item > i) acc[1].push(item);
+            return acc;
+        }, [[],[]])
+        
         const rangeObj = {
             round: i,
-            range: stages.filter(v => v === i).length / stages.filter(v => v >= i).length
+            range: rangeArr[0].length / rangeArr[1].length
+            
         }
         answer.push(rangeObj);
     }
