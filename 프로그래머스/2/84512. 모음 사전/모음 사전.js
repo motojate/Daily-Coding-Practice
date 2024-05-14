e81874a2eb5a7ad774a4c5054e54d60718f6e3cf
강대현 <120537554+motojate@users.noreply.github.com>
2024-05-14 16:47:34 +0900

    [level 2] Title: 모음 사전, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,15 +1,17 @@
 function solution(word) {
     const words = ['A', 'E', 'I', 'O', 'U'];
-    const bucket = [];
-    const dfs = (str) => {
-        if(str.length > 5) return;
-        
-        bucket.push(str);
-        
-        for(const w of words) {
-            dfs(str + w);
+    let answer = 0;
+    const calNum = (idx) => {
+        let sum = 0;
+        for(let i = 0; i <= idx; i++) {
+            sum += 5 ** i;
         }
+        return sum;
     }
-    dfs("");
-    return bucket.indexOf(word);
+    
+    for(let i = 0; i < word.length; i++) {
+        answer += words.indexOf(word[i]) * calNum(4 - i) + 1;
+    }
+    
+    return answer;
 }
