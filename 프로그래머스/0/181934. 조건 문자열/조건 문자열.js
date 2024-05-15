9937ce9c3dbd3e072bc764c82cb6fb7f1d9cdc59
강대현 <120537554+motojate@users.noreply.github.com>
2024-05-16 04:48:33 +0900

    [level 0] Title: 조건 문자열, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,7 @@
+function solution(ineq, eq, n, m) {
+    if(ineq === '<') {
+        return eq === '=' ? Number(n <= m) : Number(n < m);
+    } else {
+        return eq === '=' ? Number(n >= m) : Number(n > m);
+    }
+}
