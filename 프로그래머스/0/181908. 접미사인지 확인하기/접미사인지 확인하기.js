47ae41c98f9aec116d6ec80bc09659099546b2bc
강대현 <120537554+motojate@users.noreply.github.com>
2024-05-22 10:45:48 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -0,0 +1,9 @@
+function solution(my_string, is_suffix) {
+   
+    for(let i = 0; i < my_string.length; i++) {
+        if(is_suffix === my_string.slice(i)) return 1;
+    }
+    
+    return 0;
+
+}
