cfc188d7f7409381a6968a125ece710a5a33995a
강대현 <120537554+motojate@users.noreply.github.com>
2024-06-04 23:21:23 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
@@ -0,0 +1,8 @@
+function solution(my_string, alp) {
+    var answer = '';
+    for(const str of my_string) {
+        if(str === alp) answer += str.toUpperCase();
+        else answer += str;
+    }
+    return answer;
+}
