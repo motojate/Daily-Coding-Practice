3e69aac02e9e7f5a6478fdcbd8d5cb7ccde61ce7
강대현 <120537554+motojate@users.noreply.github.com>
2024-06-06 02:42:02 +0900

    [level 2] Title: 택배상자, Time: 14.30 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.js
@@ -0,0 +1,17 @@
+function solution(order) {
+    let answer = 0;
+    let temp = 1;
+    const stack = [];
+
+    for (let i = 0; i < order.length; i++) {
+        const current = order[i];
+
+        while (temp <= current) stack.push(temp++);
+
+        if (stack.pop() !== current) break;
+
+        answer++;
+    }
+
+    return answer;
+}
