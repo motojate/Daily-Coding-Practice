88d7c21a4dec7de968fbb6ef3788cf78d9748aef
강대현 <120537554+motojate@users.noreply.github.com>
2024-06-07 15:30:13 +0900

    [level 0] Title: 배열 만들기 6, Time: 4.39 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
@@ -0,0 +1,8 @@
+function solution(arr) {
+    const stk = [];
+    for(let i = 0; i < arr.length; i++) {
+        if(stk[stk.length - 1] === arr[i]) stk.pop();
+        else stk.push(arr[i]);
+    }
+    return stk.length ? stk : [-1];
+}
