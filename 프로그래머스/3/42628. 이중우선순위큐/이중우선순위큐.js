31398b7352db1fddaa7c8f9697d285ee31100b4c
강대현 <120537554+motojate@users.noreply.github.com>
2024-06-12 02:56:03 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.43 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.js
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.js
@@ -1,18 +1,133 @@
-function solution(arg) {
-    var list = [];
-    arg.forEach(t=>{
-        if(t[0] === 'I'){
-            list.push(+(t.split(" ")[1]));            
+function solution(operations) {
+    const minHeap = new MinHeap();
+    const maxHeap = new MaxHeap();
+    
+    for(const operation of operations) {
+        const [op, data] = operation.split(' ');
+        
+        if(op === 'I') {
+            minHeap.insert(+data);
+            maxHeap.insert(+data);
+        } else {
+            if(minHeap.size() === 0) continue;
+            
+            if(+data === 1) {
+                maxHeap.pop();
+                minHeap.reset();
+                for(const heapData of maxHeap.heap) {
+                    minHeap.insert(heapData);
+                }
+            } else {
+                minHeap.pop();
+                maxHeap.reset();
+                for(const heapData of minHeap.heap) {
+                    maxHeap.insert(heapData);
+                }
+            }
         }
-        else{
-            if(!list.length) return;
+    }
+    if(minHeap.size() === 0 && maxHeap.size() === 0) return [0, 0];
+    else return [maxHeap.pop(), minHeap.pop()];
+}
 
-            var val = (t[2] === '-' ? Math.min : Math.max)(...list);
-            var delIndex = list.findIndex(t=> t ===  val);
-
-            list.splice(delIndex, 1);
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+    
+    insert(data) {
+        this.heap.push(data);
+        let current = this.heap.length - 1;
+        let parent = Math.floor((current - 1) / 2);
+        while(this.heap[current] < this.heap[parent]) {
+            this.swap(current, parent);
+            current = parent;
+            parent = Math.floor((current - 1) / 2);
         }
-    })
+    }
+    
+    pop() {
+        this.swap(0, this.heap.length - 1);
+        const value = this.heap.pop();
+        
+        let current = 0;
+        let left = current * 2 + 1;
+        let right = current * 2 + 2;
+        while(this.heap[current] > this.heap[left] || this.heap[current] > this.heap[right]) {
+            if(this.heap[left] < this.heap[right]) {
+                this.swap(current, left);
+                current = left;
+            } else {
+                this.swap(current, right);
+                current = right;
+            }
+            left = current * 2 + 1;
+            right = current * 2 + 2;
+        }
+        
+        return value;
+    }
+    
+    size() {
+        return this.heap.length;
+    }
+    
+    reset() {
+        this.heap = [];
+    }
+    
+    swap(i, j) {
+        [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+    }
+}
 
-    return list.length ? [Math.max(...list), Math.min(...list)] : [0, 0];
+class MaxHeap {
+    constructor() {
+        this.heap = [];
+    }
+    
+    insert(data) {
+        this.heap.push(data);
+        let current = this.heap.length - 1;
+        let parent = Math.floor((current - 1) / 2);
+        while(this.heap[current] > this.heap[parent]) {
+            this.swap(current, parent);
+            current = parent;
+            parent = Math.floor((current - 1) / 2);
+        }
+    }
+    
+    pop() {
+        this.swap(0, this.heap.length - 1);
+        const value = this.heap.pop();
+        
+        let current = 0;
+        let left = current * 2 + 1;
+        let right = current * 2 + 2;
+        while(this.heap[current] < this.heap[left] || this.heap[current] < this.heap[right]) {
+            if(this.heap[left] > this.heap[right]) {
+                this.swap(current, left);
+                current = left;
+            } else {
+                this.swap(current, right);
+                current = right;
+            }
+            left = current * 2 + 1;
+            right = current * 2 + 2;
+        }
+        
+        return value;
+    }
+    
+    size() {
+        return this.heap.length;
+    }
+    
+    reset() {
+        this.heap = [];
+    }
+    
+    swap(i, j) {
+        [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+    }
 }
