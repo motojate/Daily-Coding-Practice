66f9d39343d7a26d0d9cde9e691b72e290131617
강대현 <120537554+motojate@users.noreply.github.com>
2024-06-14 01:06:25 +0900

    [level 2] Title: [3차] n진수 게임, Time: 4.35 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
@@ -4,17 +4,19 @@ function solution(n, t, m, p) {
     let order = 1;
     
     const generateNumber = (num) => {
-        return num.toString(n);
+        return num.toString(n).toUpperCase();
     }
     
     while(answer.length < t) {
         const num = generateNumber(i++);
         for(const str of num) {
-            if(order === p) answer += str.toUpperCase();
-            if(answer.length === t) break;
+            if(order === p) {
+                answer += str;
+                if(answer.length === t) return answer;
+            }
             
-            order++;
-            if(order > m) order %= m;
+            order = order % m + 1;
+
         }
     }
     
