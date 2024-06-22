5d6fbe934b895cec1a3f83bcc741ae4581182c03
강대현 <120537554+motojate@users.noreply.github.com>
2024-06-22 23:46:59 +0900

    [Bronze V] Title: 꼬마 정민, Time: 144 ms, Memory: 9448 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.js
@@ -0,0 +1,16 @@
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+readline
+  .on("line", function (line) {
+    input = line.split(" ").map((v) => parseInt(v));
+  })
+  .on("close", function () {
+    const [A, B, C] = input;
+    console.log(A + B + C);
+    process.exit();
+  });
