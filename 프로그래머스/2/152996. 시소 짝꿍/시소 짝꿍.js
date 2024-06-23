c171fff2a9b3753112cc6abd8c775cc88c608138
강대현 <120537554+motojate@users.noreply.github.com>
2024-06-23 11:36:37 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
@@ -1,19 +1,19 @@
 function solution(weights) {
     const weightMap = new Map();
-    const weightArr = [1/2, 3/4, 2/3, 3/2, 4/3, 2];
+    const weightRateArr = [1/2, 3/4, 2/3, 3/2, 4/3, 2];
     let answer = 0;
     
     for(const weight of weights) {
         const data = weightMap.get(weight) ?? 0;
         answer += data; // 같은 경우.
         
-        for(const w of weightArr) {
-            const wData = weightMap.get(weight * w);
-            if(wData) answer += wData;
+        for(const rate of weightRateArr) {
+            const rateData = weightMap.get(weight * rate);
+            if(rateData) answer += rateData;
         }
-    
-        weightMap.set(weight, data + 1);
         
+        weightMap.set(weight, data + 1);
     }
+    
     return answer;
 }
