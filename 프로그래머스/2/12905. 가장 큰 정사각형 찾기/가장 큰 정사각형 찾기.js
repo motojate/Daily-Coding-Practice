d9db0c002fb798f659f760dee45a0a551932d423
강대현 <120537554+motojate@users.noreply.github.com>
2024-07-01 21:11:44 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 54.04 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.js
@@ -0,0 +1,16 @@
+function solution(board) {
+    let answer = 0;
+    if(board[0][0] === 1) answer = 1;
+    
+    for(let i = 1; i < board.length; i++) {
+        for(let j = 1; j < board[i].length; j++) {
+            if(board[i][j] === 0) continue;
+            
+            board[i][j] = Math.min(board[i - 1][j], board[i][j - 1], board[i - 1][j - 1]) + 1;
+            
+            answer = Math.max(answer, board[i][j]);
+        }
+    }
+    
+    return answer * answer;
+}
