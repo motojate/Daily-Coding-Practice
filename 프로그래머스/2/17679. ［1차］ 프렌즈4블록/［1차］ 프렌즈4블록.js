ad1b69340acc6641eb7d0480837a7e9bb2d9150a
강대현 <120537554+motojate@users.noreply.github.com>
2024-07-02 17:17:33 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 2.79 ms, Memory: 34 MB -BaekjoonHub

---
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -0,0 +1,61 @@
+function solution(m, n, board) {
+    board = board.map((v) => v.split(''));
+    const col = board[0].length;
+    const row = board.length;
+    let answer = 0;
+    
+    const deleteBlock = () => {
+        for(let i = 0; i < col; i++) {
+            let rowData = '';
+            for(let j = 0; j < row; j++) {
+                rowData += board[j][i];
+            }
+            const zeroReplaceStr = replaceZero(rowData);
+            for(let k = 0; k < row; k++) {
+                board[k][i] = zeroReplaceStr[k];
+            }
+        }
+    }
+    
+   
+    const directions = [[0, 1], [1, 0], [1, 1]];
+    
+    const isValidSquare = (i, j, data) => {
+        return board[i + 1][j] === data && board[i][j + 1] === data && board[i + 1][j + 1] === data;
+    }
+    
+    const blockBoom = () => {
+        const deleteSet = new Set();
+        for(let i = 0; i < row - 1; i++) {
+            for(let j = 0; j < col - 1; j++) {
+                const data = board[i][j];
+                if(data === '0') continue;
+                if(isValidSquare(i, j, data)) {
+                    for(let ni = 0; ni <= 1; ni++) {
+                        for(let nj = 0; nj <= 1; nj++) {
+                            const str = `${i + ni}-${j + nj}`;
+                            deleteSet.add(str);
+                        }
+                    }
+                } 
+            }
+        }
+        if(deleteSet.size > 0) {
+            answer += deleteSet.size;
+            for(const set of deleteSet) {
+                const [i, j] = set.split('-').map(Number);
+                board[i][j] = '0';
+            }
+            deleteBlock();
+            blockBoom();
+        }
+    }
+    
+    blockBoom();
+    return answer;
+}
+
+const replaceZero = (str) => {
+    const nonZeroPart = str.replace(/0/g, '');
+    return '0'.repeat(str.length - nonZeroPart.length) + nonZeroPart; // '0'들을 앞에 붙이고 나머지 문자열을 뒤에 붙임
+}
