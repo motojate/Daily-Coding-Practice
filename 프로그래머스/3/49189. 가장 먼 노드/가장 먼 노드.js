30af8fc8b49991d15b909b9f20c4e95753d9730a
강대현 <120537554+motojate@users.noreply.github.com>
2024-07-03 23:00:35 +0900

    [level 3] Title: 가장 먼 노드, Time: 59.27 ms, Memory: 55.2 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -1,17 +1,15 @@
 function solution(n, edge) {
     const visited = Array.from({ length: n + 1 }).fill(false);
     const level = Array.from({ length: n + 1 }).fill(0);
+    const adjList = Array.from({ length: n + 1 }, () => []);
+    
+    for(const [u, v] of edge) {
+        adjList[u].push(v);
+        adjList[v].push(u);
+    }
     
     const queue = [1];
     visited[1] = true;
-
-    const visitNode = (node, head, lev) => {
-        if(!visited[node]) {
-            queue.push(node);
-            visited[node] = true;
-            level[node] = lev;
-        }
-    }
     
     while(queue.length > 0) {
         const size = queue.length;
@@ -19,9 +17,12 @@ function solution(n, edge) {
         for(let i = 0; i < size; i++) {
             const head = queue.shift();
             const lev = level[head] + 1;
-            for(const [n1, n2] of edge) {
-                if(n1 === head) visitNode(n2, head, lev);
-                if(n2 === head) visitNode(n1, head, lev);
+            for(const node of adjList[head]) {
+                if(!visited[node]) {
+                    queue.push(node);
+                    visited[node] = true;
+                    level[node] = lev;
+                }
             }
             
         }
